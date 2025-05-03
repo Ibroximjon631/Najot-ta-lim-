2537aab756bc1e30acb3d6f56bc2694db0de3a3b
ibroximjon631 <ibroximjon631@gmail.com>
2025-05-03 10:17:35 +0500

    iii

---
+++ iiabcdef.py
@@ -0,0 +1,9 @@
+def abcdef(abc:str):
+    lst=""
+    if not abc:
+        return "Nmadr yozng"
+    else:
+        for i in range(0,len(abc),2):
+            iii=abc[i+1]
+            lst+=str(int(abc[i])*iii)
+        return lst
